11106f5842cd430e94e619d8bed211959976f42f
s-horii <s2010310horii@gmail.com>
2021-05-10 10:35:31 +0900

    inject API_URL to window

--- src/controller.rs
+++ src/controller.rs
@@ -4,11 +4,48 @@ use crate::repository;
 use actix_web::{delete, error, get, post, web, HttpRequest, HttpResponse};
 use anyhow::bail;
 use asset::dir::AssetDir;
+use html5ever::driver::ParseOpts;
+use html5ever::parse_document;
+use html5ever::serialize;
+use html5ever::serialize::SerializeOpts;
+use html5ever::tendril::{StrTendril, TendrilSink};
+use html5ever::{namespace_url, ns};
+use html5ever::{Attribute, LocalName, QualName};
+use markup5ever_rcdom::{Node, NodeData, RcDom, SerializableHandle};
+use std::cell::RefCell;
 use std::path::PathBuf;
 
 pub mod ui {
     use super::*;
     const PATH_PREFIX: &'static str = "build";
+
+    fn inject_script(value: &str) -> String {
+        let parser = parse_document(RcDom::default(), ParseOpts::default());
+        let dom = parser.one(value);
+        {
+            let html_node = &dom.document.children.borrow()[1];
+            let script_node = Node::new(NodeData::Element {
+                name: QualName::new(None, ns!(html), LocalName::from("script")),
+                attrs: RefCell::new(vec![Attribute {
+                    name: QualName::new(None, ns!(), LocalName::from("type")),
+                    value: StrTendril::from("text/javascript"),
+                }]),
+                template_contents: None,
+                mathml_annotation_xml_integration_point: false,
+            });
+            script_node
+                .children
+                .borrow_mut()
+                .push(Node::new(NodeData::Text {
+                    contents: RefCell::new(format!("window.API_URL='{}';", "0.0.0.0:8080").into()),
+                }));
+            &html_node.children.borrow_mut().push(script_node);
+        }
+        let doc: SerializableHandle = dom.document.into();
+        let mut bytes = vec![];
+        serialize(&mut bytes, &doc, SerializeOpts::default()).unwrap();
+        String::from_utf8(bytes).unwrap()
+    }
 
     #[get("{filepath:.*}")]
     pub async fn get(
@@ -24,11 +61,16 @@ pub mod ui {
             .unwrap_or(PathBuf::from("index.html"));
         path.push(subpath);
         let file_ = directory
-            .find(path)
+            .find(&path)
             .map_err(|e| error::ErrorBadRequest(e))?;
         let value = String::from_utf8(file_.content())
             .map_err(|_e| error::ErrorBadRequest(file_.name()))?;
-        Ok(HttpResponse::Ok().body(value))
+        if path.ends_with(PathBuf::from("index.html")) {
+            let index = inject_script(&value.clone());
+            Ok(HttpResponse::Ok().body(index))
+        } else {
+            Ok(HttpResponse::Ok().body(value))
+        }
     }
 }
 
